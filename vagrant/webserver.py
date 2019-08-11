5cae4e033f2d56207be1dabf00fef7e80478b481
zilhsoj <zilhsoj@gmail.com>
2019-08-11 16:00:58 -0400

    Added a new restaurant

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -19,11 +19,10 @@ class webServerHandler(BaseHTTPRequestHandler):
         try:
             if self.path.endswith("/restaurants"):
                 restaurants = session.query(Restaurant).all()
+
                 output = ""
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
                 output += "<html><body>"
+                output += "<a href = '/restaurants/new'> Make a New Restaurant Here </a>"
                 for restaurant in restaurants:
                     output += restaurant.name
                     output += "</br>"
@@ -32,12 +31,48 @@ class webServerHandler(BaseHTTPRequestHandler):
                     output += "</br>"
                     output += "<a href =' #'> Delete </a>"
                     output += "</br></br></br>"
-
                 output += "</body></html>"
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(output)
+                return
+
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "Make a New Restaurant </br></br>"
+                output += "<form action = '/restauarnts/new' enctype='multipart/form-data' method = 'POST'><input type='text' name = 'newRestaurantName' placeholder='New Restaurant Here'>"
+                output += "<input type = 'submit'>"
+                output += "</html></body>"
                 self.wfile.write(output)
                 return
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
+
+    def do_POST(self):
+        try:
+            if self.path.endswith('/restaurants/new'):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                newRestaurant = Restaurant(name=messageconent[0])
+                session.add(newRestaurant)
+                session.commit()
+                self.send_response(302)
+                self.send_header('content-type', 'text-html')
+                self.send_header('location', '/restaurants')
+                self.end_headers()
+                return
+
+        except:
+            pass
 
 
 def main():
