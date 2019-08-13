75d0e746bb1618c1df0a041324ee60bc714f95ed
Josh Li <zilhsoj@gmail.com>
2019-08-13 10:16:20 -0400

    Wrote and Tested edit restaurant name.

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -30,7 +30,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                     output += restaurant.name
                     output += "</br>"
                     # Objective 2 -- Add Edit and Delete Links
-                    output += "<a href ='#' >Edit </a> "
+                    output += "<a href ='/restaurants/%s/edit'>Edit</a>" % restaurant.id
                     output += "</br>"
                     output += "<a href =' #'> Delete </a>"
                     output += "</br></br></br>"
@@ -53,6 +53,27 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 return
 
+            if self.path.endswith("/edit"):
+                restaurantID = self.path.split('/')[2]
+                restaurantQuery = session.query(Restaurant).filter_by(id=restaurantID).one()
+                # Need to check if it's empty, otherwise the name will be updated to empty with a click
+                if restaurantQuery != []:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = ""
+                    output += "<html><body>"
+                    output += "<h1>"
+                    output += restaurantQuery.name
+                    output += "</h1>"
+                    output += "<form method = 'POST' action = '/restaurants/%s/edit' enctype='multipart/form-data'>" % restaurantID
+                    output += "<input name = 'updateRestaurantName' type='text' placeholder='%s'>" % restaurantQuery.name
+                    output += "<input type = 'submit' value ='Rename'>"
+                    # if click 'submit', the form-data will be sent to the action page.
+                    output += "</form></body></html>"
+                    self.wfile.write(output)
+                return
+
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
@@ -66,11 +87,25 @@ class webServerHandler(BaseHTTPRequestHandler):
                     newRestaurant = Restaurant(name=messageContent[0])
                     session.add(newRestaurant)
                     session.commit()
-
                     self.send_response(301)
                     self.send_header('Content-type', 'text-html')
                     self.send_header('Location', '/restaurants')
                     self.end_headers()
+            if self.path.endswith('/edit'):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messageContent = fields.get('updateRestaurantName')
+                    restaurantID = self.path.split('/')[2]
+                    restaurantQuery = session.query(Restaurant).filter_by(id=restaurantID).one()
+                    if restaurantQuery != []:
+                        restaurantQuery.name = messageContent[0]
+                        session.add(restaurantQuery)
+                        session.commit
+                        self.send_response(302)
+                        self.send_header('Content-type', 'text-html')
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
 
         except IOError:
             pass
